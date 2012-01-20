96badbf5b04fdb8302e06476308d36d0cb9c94af
François Germain <cahnory@gmail.com>
2012-01-20 14:22:41 +0100

    Better console support and different alert types
    + warning are sent instead of log when using console
    + error are sent instead of log when using undefined framework
    + All console methods are watched (previously only log was)

--- src/jsDebug.js
+++ src/jsDebug.js
@@ -1,15 +1,49 @@
 (function(window){
-	var	jsDebug 		= this;
+	var	jsDebug 		= {},
+		keepAlerting	= true;
 	
 	// Console
 	var consoleReady	= typeof window.console !== 'undefined',
-		consoleLog		= consoleReady ? window.console.log : function(){},
-		keepAlerting	= true;
+		consoleMethods	= ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'trace', 'warn'],
+		//s:source object, t:target object, n:method name, w:warning message 
+		wrapMethod		= function(s, t, n, w) {
+			var m = s[n];
+			t[n]	= function() {
+				m.apply(s, arguments);
+			}
+			s[n] = function() {
+				jsDebug.warning(w);
+				m.apply(s, arguments);
+			}
+		};
+	
 	if(!consoleReady) {
-		window.console	= {log:function(){}};
+		window.console	= {
+			assert: function() {},
+			count: function() {},
+			debug: function() {},
+			dir: function() {},
+			dirxml: function() {},
+			error: function() {},
+			exception: function() {},
+			group: function() {},
+			groupCollapsed: function() {},
+			groupEnd: function() {},
+			info: function() {},
+			log: function() {},
+			profile: function() {},
+			profileEnd: function() {},
+			table: function() {},
+			time: function() {},
+			timeEnd: function() {},
+			trace: function() {},
+			warn: function() {}
+		};
 	}
-	jsDebug.log		= function() {
-		consoleLog.apply(window.console, arguments);
+	
+	jsDebug.console	= {};
+	for(var i in consoleMethods) {
+		wrapMethod(window.console, jsDebug.console, consoleMethods[i], 'Using console could stop your script in IE');
 	}
 	jsDebug.alert	= function(error) {
 		if(keepAlerting) {
@@ -18,14 +52,17 @@
 	}
 	jsDebug.error	= function(error) {
 		if(!consoleReady) {
-			alert('jsDebug: '+error);
+			jsDebug.alert('jsDebug: '+error);
 		} else {
-			jsDebug.log('jsDebug: '+error);
+			jsDebug.console.error('jsDebug: '+error);
 		}
 	}
-	window.console.log = function() {
-		jsDebug.error('Using console could stop your script in IE');
-		jsDebug.log.apply(this, arguments);
+	jsDebug.warning	= function(error) {
+		if(!consoleReady) {
+			jsDebug.alert('jsDebug: '+error);
+		} else {
+			jsDebug.console.warn('jsDebug: '+error);
+		}
 	}
 	
 	// Framework
